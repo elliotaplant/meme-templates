8f9789afdfc67a13d84ead051c6abe70a9fd9160
Elliot Plant <elliotaplant@gmail.com>
2021-04-23 14:57:22 -0700

    Two images on one canvas

---
+++ classes/Insertion.js
@@ -0,0 +1,8 @@
+class Insertion {
+  constructor(imageData, image, oriPoints, dstPoints) {
+    this.imageData = imageData;
+    this.image = image;
+    this.oriPoints = oriPoints;
+    this.dstPoints = dstPoints;
+  }
+}
